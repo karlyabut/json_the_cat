995c6cebf5f76207c5f6b9c09c487cc27a022099
karlyabut <karleisenyabut@gmail.com>
2019-08-29 18:36:33 +0000

    implemented callbacks/module and test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const request = require("request");
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log("UH OH:", error);
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("breed not found");
-  } else {
-    console.log(typeof data);
-    console.log(data);
-    console.log(data[0].name);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, (error, "CANT FIND BREED"));
+    } else {
+      callback(null, (error, data[0].description));
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
